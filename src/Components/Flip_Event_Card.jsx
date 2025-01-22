6126c91ad23bb5720b93f5ab2dcd2b3a6658a2a9
Kartik Garg <gargkartik503@gmail.com>
2025-01-22 15:17:44 +0530

    Mobile Responsive !

--- src/Components/Flip_Event_Card.jsx
+++ src/Components/Flip_Event_Card.jsx
@@ -41,39 +41,39 @@ const RedCard = (props) => {
 
                 {/* Front Card */}
                 <div className='front'>
-                    <div className="h-[76vh] w-[30vw] redcard rounded-2xl p-[5%] flex flex-col gap-10" >
-                        <div className='flex flex-col gap-2 h-[35%]'>
+                    <div className="h-[25vh] sm:h-[40vh] md:h-[47vh] lg:h-[65vh] xl:h-[76vh] w-[30vw] redcard rounded-2xl p-[5%] flex flex-col gap-[8%]" >
+                        <div className='flex flex-col gap-[5%] h-[35%]'>
                             <div>
                                 <h1 className="text-nowrap text-[2.6vw] font-bold">{props.headUp}</h1>
                                 <h1 className="text-nowrap text-[2.6vw] font-bold">{props.headDown}</h1>
                             </div>
                             <div className='flex items-center justify-start gap-4'>
-                                <span className='px-[1.5vw] py-[0.1vw] border-black border-[1.6px] scale-y-105 rounded-full text-[1.15vw] uppercase'>{props.Box1}</span>
-                                <span className='px-[1.5vw] py-[0.1vw] border-black border-[1.6px] rounded-full scale-y-105 text-[1.15vw] uppercase'>{props.Box2}</span>
+                                <span className='px-[1.5vw] py-[0.1vw] border-black border-[0.2px] text-center md:border-[1.6px] scale-y-105 rounded-full text-[1.15vw] uppercase'>{props.Box1}</span>
+                                <span className='px-[1.5vw] py-[0.1vw] border-black border-[0.2px] text-center md:border-[1.6px] rounded-full scale-y-105 text-[1.15vw] uppercase'>{props.Box2}</span>
                             </div>
                         </div>
 
                         <div className='h-[65%]'>
-                            <img src={props.ImgLink} alt="" className='border-black border-[1.6px] z-50 w-[99.5%] h-[100%] rounded-2xl' />
+                            <img src={props.ImgLink} alt="" className='border-black border-[0.3px] md:border-[1.6px] z-50 w-[99.5%] h-[100%] rounded-2xl' />
                         </div>
                     </div>
                 </div>
 
                 {/* Back Card */}
                 <div className='back absolute top-0'>
-                    <div className="h-[76vh] w-[30vw] bg-[#333] rounded-2xl p-[5%] text-[#e7e7e7] flex flex-col gap-10" >
-                        <div className='flex flex-col gap-2 h-[35%]'>
+                    <div className="h-[25vh] sm:h-[32vh] md:h-[47vh] lg:h-[65vh] xl:h-[76vh] w-[30vw] bg-[#333] rounded-2xl p-[5%] text-[#e7e7e7] flex flex-col gap-[8%]" >
+                        <div className='flex flex-col gap-[5%] h-[35%]'>
                             <div>
                                 <h1 className="text-nowrap text-[2.6vw] font-bold">{props.headUp}</h1>
                                 <h1 className="text-nowrap text-[2.6vw] font-bold">{props.headDown}</h1>
                             </div>
                             <div className='flex items-center justify-start gap-4'>
-                                <span className='px-[1.5vw] py-[0.1vw] border-white border-[1.6px] scale-y-105 rounded-full text-[1.15vw] uppercase'>{props.Box1}</span>
-                                <span className='px-[1.5vw] py-[0.1vw] border-white border-[1.6px] rounded-full scale-y-105 text-[1.15vw] uppercase'>{props.Box2}</span>
+                                <span className='px-[1.5vw] py-[0.1vw] border-white border-[0.2px] text-center md:border-[1.6px] scale-y-105 rounded-full text-[1.15vw] uppercase'>{props.Box1}</span>
+                                <span className='px-[1.5vw] py-[0.1vw] border-white border-[0.2px] text-center md:border-[1.6px] rounded-full scale-y-105 text-[1.15vw] uppercase'>{props.Box2}</span>
                             </div>
                         </div>
 
-                        <div className='flex flex-col gap-5 items-center justify-between h-[65%]'>
+                        <div className='flex flex-col gap-[2%] items-center justify-between h-[65%]'>
                             <p className='text-pretty text-[1.1vw] text-left'>{props.Para}</p>
 
                             <a href={props.link} className='w-full h-[25%]'>
@@ -127,39 +127,39 @@ const BlueCard = (props) => {
 
                 {/* Front Card */}
                 <div className='front'>
-                    <div className="h-[76vh] w-[30vw] bluecard rounded-2xl p-[5%] flex flex-col gap-10" >
-                        <div className='flex flex-col gap-2 h-[35%]'>
+                    <div className="h-[25vh] sm:h-[40vh] md:h-[47vh] lg:h-[65vh] xl:h-[76vh] w-[30vw] bluecard rounded-2xl p-[5%] flex flex-col gap-[8%]" >
+                        <div className='flex flex-col gap-[5%] h-[35%]'>
                             <div>
                                 <h1 className="text-nowrap text-[2.6vw] font-bold">{props.headUp}</h1>
                                 <h1 className="text-nowrap text-[2.6vw] font-bold">{props.headDown}</h1>
                             </div>
                             <div className='flex items-center justify-start gap-4'>
-                                <span className='px-[1.5vw] py-[0.1vw] border-black border-[1.6px] scale-y-105 rounded-full text-[1.15vw] uppercase'>{props.Box1}</span>
-                                <span className='px-[1.5vw] py-[0.1vw] border-black border-[1.6px] rounded-full scale-y-105 text-[1.15vw] uppercase'>{props.Box2}</span>
+                                <span className='px-[1.5vw] py-[0.1vw] border-black border-[0.2px] text-center md:border-[1.6px] scale-y-105 rounded-full text-[1.15vw] uppercase'>{props.Box1}</span>
+                                <span className='px-[1.5vw] py-[0.1vw] border-black border-[0.2px] text-center md:border-[1.6px] rounded-full scale-y-105 text-[1.15vw] uppercase'>{props.Box2}</span>
                             </div>
                         </div>
 
                         <div className='h-[65%]'>
-                            <img src={props.ImgLink} alt="" className='border-black border-[1.6px] z-50 w-[99.5%] h-[100%] rounded-2xl' />
+                            <img src={props.ImgLink} alt="" className='border-black border-[0.3px] md:border-[1.6px] z-50 w-[99.5%] h-[100%] rounded-2xl' />
                         </div>
                     </div>
                 </div>
 
                 {/* Back Card */}
                 <div className='back absolute top-0'>
-                    <div className="h-[76vh] w-[30vw] bg-[#333] rounded-2xl p-[5%] text-[#e7e7e7] flex flex-col gap-10" >
-                        <div className='flex flex-col gap-2 h-[35%]'>
+                    <div className="h-[25vh] sm:h-[32vh] md:h-[47vh] lg:h-[65vh] xl:h-[76vh] w-[30vw] bg-[#333] rounded-2xl p-[5%] text-[#e7e7e7] flex flex-col gap-[8%]" >
+                        <div className='flex flex-col gap-[5%] h-[35%]'>
                             <div>
                                 <h1 className="text-nowrap text-[2.6vw] font-bold">{props.headUp}</h1>
                                 <h1 className="text-nowrap text-[2.6vw] font-bold">{props.headDown}</h1>
                             </div>
                             <div className='flex items-center justify-start gap-4'>
-                                <span className='px-[1.5vw] py-[0.1vw] border-white border-[1.6px] scale-y-105 rounded-full text-[1.15vw] uppercase'>{props.Box1}</span>
-                                <span className='px-[1.5vw] py-[0.1vw] border-white border-[1.6px] rounded-full scale-y-105 text-[1.15vw] uppercase'>{props.Box2}</span>
+                                <span className='px-[1.5vw] py-[0.1vw] border-white border-[0.2px] text-center md:border-[1.6px] scale-y-105 rounded-full text-[1.15vw] uppercase'>{props.Box1}</span>
+                                <span className='px-[1.5vw] py-[0.1vw] border-white border-[0.2px] text-center md:border-[1.6px] rounded-full scale-y-105 text-[1.15vw] uppercase'>{props.Box2}</span>
                             </div>
                         </div>
 
-                        <div className='flex flex-col gap-5 items-center justify-between h-[65%]'>
+                        <div className='flex flex-col gap-[2%] items-center justify-between h-[65%]'>
                             <p className='text-pretty text-[1.1vw] text-left'>{props.Para}</p>
 
                             <a href={props.link} className='w-full h-[25%]'>
@@ -213,39 +213,39 @@ const GreenCard = (props) => {
 
                 {/* Front Card */}
                 <div className='front'>
-                    <div className="h-[76vh] w-[30vw] greencard rounded-2xl p-[5%] flex flex-col gap-10" >
-                        <div className='flex flex-col gap-2 h-[35%]'>
+                    <div className="h-[25vh] sm:h-[40vh] md:h-[47vh] lg:h-[65vh] xl:h-[76vh] w-[30vw] greencard rounded-2xl p-[5%] flex flex-col gap-[8%]" >
+                        <div className='flex flex-col gap-[5%] h-[35%]'>
                             <div>
                                 <h1 className="text-nowrap text-[2.6vw] font-bold">{props.headUp}</h1>
                                 <h1 className="text-nowrap text-[2.6vw] font-bold">{props.headDown}</h1>
                             </div>
                             <div className='flex items-center justify-start gap-4'>
-                                <span className='px-[1.5vw] py-[0.1vw] border-black border-[1.6px] scale-y-105 rounded-full text-[1.15vw] uppercase'>{props.Box1}</span>
-                                <span className='px-[1.5vw] py-[0.1vw] border-black border-[1.6px] rounded-full scale-y-105 text-[1.15vw] uppercase'>{props.Box2}</span>
+                                <span className='px-[1.5vw] py-[0.1vw] border-black border-[0.2px] text-center md:border-[1.6px] scale-y-105 rounded-full text-[1.15vw] uppercase'>{props.Box1}</span>
+                                <span className='px-[1.5vw] py-[0.1vw] border-black border-[0.2px] text-center md:border-[1.6px] rounded-full scale-y-105 text-[1.15vw] uppercase'>{props.Box2}</span>
                             </div>
                         </div>
 
                         <div className='h-[65%]'>
-                            <img src={props.ImgLink} alt="" className='border-black border-[1.6px] z-50 w-[99.5%] h-[100%] rounded-2xl' />
+                            <img src={props.ImgLink} alt="" className='border-black border-[0.3px] md:border-[1.6px] z-50 w-[99.5%] h-[100%] rounded-2xl' />
                         </div>
                     </div>
                 </div>
 
                 {/* Back Card */}
                 <div className='back absolute top-0'>
-                    <div className="h-[76vh] w-[30vw] bg-[#333] rounded-2xl p-[5%] text-[#e7e7e7] flex flex-col gap-10" >
-                        <div className='flex flex-col gap-2 h-[35%]'>
+                    <div className="h-[25vh] sm:h-[32vh] md:h-[47vh] lg:h-[65vh] xl:h-[76vh] w-[30vw] bg-[#333] rounded-2xl p-[5%] text-[#e7e7e7] flex flex-col gap-[8%]" >
+                        <div className='flex flex-col gap-[5%] h-[35%]'>
                             <div>
                                 <h1 className="text-nowrap text-[2.6vw] font-bold">{props.headUp}</h1>
                                 <h1 className="text-nowrap text-[2.6vw] font-bold">{props.headDown}</h1>
                             </div>
                             <div className='flex items-center justify-start gap-4'>
-                                <span className='px-[1.5vw] py-[0.1vw] border-white border-[1.6px] scale-y-105 rounded-full text-[1.15vw] uppercase'>{props.Box1}</span>
-                                <span className='px-[1.5vw] py-[0.1vw] border-white border-[1.6px] rounded-full scale-y-105 text-[1.15vw] uppercase'>{props.Box2}</span>
+                                <span className='px-[1.5vw] py-[0.1vw] border-white border-[0.2px] text-center md:border-[1.6px] scale-y-105 rounded-full text-[1.15vw] uppercase'>{props.Box1}</span>
+                                <span className='px-[1.5vw] py-[0.1vw] border-white border-[0.2px] text-center md:border-[1.6px] rounded-full scale-y-105 text-[1.15vw] uppercase'>{props.Box2}</span>
                             </div>
                         </div>
 
-                        <div className='flex flex-col gap-5 items-center justify-between h-[65%]'>
+                        <div className='flex flex-col gap-[2%] items-center justify-between h-[65%]'>
                             <p className='text-pretty text-[1.1vw] text-left'>{props.Para}</p>
 
                             <a href={props.link} className='w-full h-[25%]'>
@@ -299,39 +299,39 @@ const YellowCard = (props) => {
 
                 {/* Front Card */}
                 <div className='front'>
-                    <div className="h-[76vh] w-[30vw] yellowcard rounded-2xl p-[5%] flex flex-col gap-10" >
-                        <div className='flex flex-col gap-2 h-[35%]'>
+                    <div className="h-[25vh] sm:h-[40vh] md:h-[47vh] lg:h-[65vh] xl:h-[76vh] w-[30vw] yellowcard rounded-2xl p-[5%] flex flex-col gap-[8%]" >
+                        <div className='flex flex-col gap-[5%] h-[35%]'>
                             <div>
                                 <h1 className="text-nowrap text-[2.6vw] font-bold">{props.headUp}</h1>
                                 <h1 className="text-nowrap text-[2.6vw] font-bold">{props.headDown}</h1>
                             </div>
                             <div className='flex items-center justify-start gap-4'>
-                                <span className='px-[1.5vw] py-[0.1vw] border-black border-[1.6px] scale-y-105 rounded-full text-[1.15vw] uppercase'>{props.Box1}</span>
-                                <span className='px-[1.5vw] py-[0.1vw] border-black border-[1.6px] rounded-full scale-y-105 text-[1.15vw] uppercase'>{props.Box2}</span>
+                                <span className='px-[1.5vw] py-[0.1vw] border-black border-[0.2px] text-center md:border-[1.6px] scale-y-105 rounded-full text-[1.15vw] uppercase'>{props.Box1}</span>
+                                <span className='px-[1.5vw] py-[0.1vw] border-black border-[0.2px] text-center md:border-[1.6px] rounded-full scale-y-105 text-[1.15vw] uppercase'>{props.Box2}</span>
                             </div>
                         </div>
 
                         <div className='h-[65%]'>
-                            <img src={props.ImgLink} alt="" className='border-black border-[1.6px] z-50 w-[99.5%] h-[100%] rounded-2xl' />
+                            <img src={props.ImgLink} alt="" className='border-black border-[0.3px] md:border-[1.6px] z-50 w-[99.5%] h-[100%] rounded-2xl' />
                         </div>
                     </div>
                 </div>
 
                 {/* Back Card */}
                 <div className='back absolute top-0'>
-                    <div className="h-[76vh] w-[30vw] bg-[#333] rounded-2xl p-[5%] text-[#e7e7e7] flex flex-col gap-10" >
-                        <div className='flex flex-col gap-2 h-[35%]'>
+                    <div className="h-[25vh] sm:h-[32vh] md:h-[47vh] lg:h-[65vh] xl:h-[76vh] w-[30vw] bg-[#333] rounded-2xl p-[5%] text-[#e7e7e7] flex flex-col gap-[8%]" >
+                        <div className='flex flex-col gap-[5%] h-[35%]'>
                             <div>
                                 <h1 className="text-nowrap text-[2.6vw] font-bold">{props.headUp}</h1>
                                 <h1 className="text-nowrap text-[2.6vw] font-bold">{props.headDown}</h1>
                             </div>
                             <div className='flex items-center justify-start gap-4'>
-                                <span className='px-[1.5vw] py-[0.1vw] border-white border-[1.6px] scale-y-105 rounded-full text-[1.15vw] uppercase'>{props.Box1}</span>
-                                <span className='px-[1.5vw] py-[0.1vw] border-white border-[1.6px] rounded-full scale-y-105 text-[1.15vw] uppercase'>{props.Box2}</span>
+                                <span className='px-[1.5vw] py-[0.1vw] border-white border-[0.2px] text-center md:border-[1.6px] scale-y-105 rounded-full text-[1.15vw] uppercase'>{props.Box1}</span>
+                                <span className='px-[1.5vw] py-[0.1vw] border-white border-[0.2px] text-center md:border-[1.6px] rounded-full scale-y-105 text-[1.15vw] uppercase'>{props.Box2}</span>
                             </div>
                         </div>
 
-                        <div className='flex flex-col gap-5 items-center justify-between h-[65%]'>
+                        <div className='flex flex-col gap-[2%] items-center justify-between h-[65%]'>
                             <p className='text-pretty text-[1.1vw] text-left'>{props.Para}</p>
 
                             <a href={props.link} className='w-full h-[25%]'>
