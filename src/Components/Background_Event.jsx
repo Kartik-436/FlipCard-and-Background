07dec9d56e1dc25896bdc84581e2cad2d36d8fe0
Kartik Garg <gargkartik503@gmail.com>
2025-01-22 18:28:14 +0530

    small grid for mobiles

--- src/Components/Background_Event.jsx
+++ src/Components/Background_Event.jsx
@@ -25,7 +25,7 @@ const Bg = () => {
         <React.Fragment>
             <div className="h-screen w-screen overflow-hidden bg-[#111]">
                 <div
-                    className="grid h-screen w-full grid-cols-[repeat(auto-fit,_minmax(100px,_1fr))] grid-rows-[repeat(auto-fit,_minmax(100px,_1fr))]"
+                    className="grid h-screen w-full grid-cols-[repeat(auto-fit,_minmax(50px,_1fr))] grid-rows-[repeat(auto-fit,_minmax(50px,_1fr))] md:grid-cols-[repeat(auto-fit,_minmax(100px,_1fr))] md:grid-rows-[repeat(auto-fit,_minmax(100px,_1fr))]"
                 >
                     {Array.from({ length: 105 }).map((_, index) => (
                         <div
